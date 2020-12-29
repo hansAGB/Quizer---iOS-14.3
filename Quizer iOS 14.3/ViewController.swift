71f94066ed440a9b445c2c13b300db83d00cd796
hansAGB <hans.aangeenbrug@icloud.com>
2020-12-29 22:33:03 +0100

    Initial Commit

---
+++ Quizer iOS 14.3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Quizer iOS 14.3
+//
+//  Created by Hans Aangeenbrug on 29/12/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
